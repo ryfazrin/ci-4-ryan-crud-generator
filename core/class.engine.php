933b252b2b4fc2b7d6654658a56292aa4be9e56c
ryfazrin <riansera515@gmail.com>
2021-08-11 22:25:38 +0800

    add core engine class

---
+++ core/class.engine.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * My Ryan CI-4 CRUD Generator
+ * Inspired: adel.qusay */ 
+
+ class Engine
+ {
+   Public $config;
+
+   function __construct()
+   {
+     # code...
+   }
+
+   function getDatabase()
+   {
+     # code...
+   }
+
+   function getTablesByDatabase()
+   {
+     # code...
+   }
+
+   function getPrimaryColumnByTable()
+   {
+     # code...
+   }
+
+   function getColumnsByTable()
+   {
+     # code...
+   }
+
+   function generate()
+   {
+     # code...
+   }
+
+  //  ===================
+
+   function getBetween()
+   {
+     # code...
+   }
+
+   function snakeToCamel()
+   {
+     # code...
+   }
+
+   function colToLabel()
+   {
+     # code...
+   }
+
+   function sanitize()
+   {
+     # code...
+   }
+ }
+ 
