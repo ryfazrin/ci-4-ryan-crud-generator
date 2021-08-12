9cb7d6c31a824f4f154c4d753eb54444ca15dd00
ryfazrin <riansera515@gmail.com>
2021-08-12 11:18:32 +0800

    add function get all column

--- core/class.engine.php
+++ core/class.engine.php
@@ -52,6 +52,7 @@
       die($tablesListHtml);
     }
 
+    // Get Primary Column
     public function getPrimaryColumnByTable($_FPOST)
     {
       $_POST = $this->sanitize($_FPOST); 
@@ -68,14 +69,85 @@
       die ($primaryColumnsListHtml);
     }
 
-    public function getColumnsByTable()
+    // Get All Column
+    public function getColumnsByTable($_FPOST)
     {
-      # code...
+      $_POST = $this->sanitize($_FPOST);
+
+      $result = $this->createQuery("DESC ".$_POST['table']);
+      $pkResult = mysqli_fetch_array($this->createQuery('SHOW KEYS FROM '.$_POST['table'].' WHERE Key_name = \'PRIMARY\''));
+
+      $columnsListHtml = '<ul class="list-group>';
+
+      while ($column = mysqli_fetch_array($result)) {
+        $disabled = ( $column[0] == trim($pkResult['Column_name']))? 'disabled' : '';
+        $columnsListHtml .= '<li class="list-group-item list-group-item-action d-flex justify-content-between align-items-center"><div class="row">
+          <input type="text" name="column[]" id="column" class="form-control" value="'.$column[0].'" placeholder="" maxlength="50" hidden>
+          <div class="col-md-2">
+            <div class="form-group">
+              <label for="label" class="form-label">Label:</label>
+              <input type="text" name="label[]" id="label" class="form-control" value="'.$this->colToLabel($column[0]).'" placeholder="" maxlength="50" required>
+            </div>
+          </div>	
+          <div class="col-md-2">
+            <div class="form-group">
+              <label for="name" class="form-label">Name/ID:</label>
+              <input type="text" name="name[]" id="name" class="form-control" value="'.$this->snakeToCamel($column[0]).'" placeholder="" maxlength="50" required>
+            </div>
+          </div>	
+          <div class="col-md-2">
+            <div class="form-group">
+              <label for="iType" class="form-label">Input type:</label>
+              <select class="form-control" name="iType[]" id="iType">
+                <option value="1" '.(strpos($column[1], 'var') !== false ? 'selected="selected"' : "").'>Text field</option>
+                <option value="2" '.(strpos($column[1], 'int') !== false ? 'selected="selected"' : "").'>Number field</option>
+                <option value="3" '.(strpos($column[0], 'pass') !== false ? 'selected="selected"' : "").'>Password field</option>
+                <option value="4">Email field</option>
+                <option value="5" '.(strpos($column[1], 'text') !== false ? 'selected="selected"' : "").'>Text area</option>
+                <option value="6">Select</option>
+                <option value="7" '.(strpos($column[1], 'date') !== false ? 'selected="selected"' : "").'>Date</option>
+              </select>
+            </div>
+          </div>									
+          <div class="col-md-2">
+            <div class="form-group">
+              <label for="maxlength" class="form-label">Max length:</label>
+              <input type="number" name="maxlength[]" id="maxlength" class="form-control" value="'. $this->getBetween($column[1], '(', ')').'" placeholder="" number="true" maxlength="50">
+            </div>
+          </div>							
+          <div class="col-md-2">
+            <div class="form-group">
+              <label for="required" class="form-label">Required:</label>
+              <select class="form-control" name="required[]" id="required" required>
+                <option value="1" '.(strpos($column[2], 'NO') !== false ? 'selected="selected"' : "").'>Yes</option>
+                <option value="0" '.(strpos($column[2], 'YES') !== false ? 'selected="selected"' : "").'>No</option>
+              </select>
+            </div>
+          </div>	
+          <div class="col-md-1">
+            <div class="form-group">
+              <label for="dtShow" class="form-label">DT:</label>
+              <select class="form-control" name="dtShow[]" id="dtShow" required>
+                <option value="1" selected>Y</option>
+                <option value="0">N</option>
+              </select>
+            </div>
+          </div>
+          <div class="col-md-1">
+            <div class="form-group">
+              <a class="btn btn-danger mt-4 '.$disabled.'" href="#"><i class="fa fa-trash"></i></a>
+            </div>
+          </div>
+        </div>
+        </li>';
+      }
+
+      die($columnsListHtml.'</ul>');
     }
 
-    public function generate()
+    public function generate($_FPOST)
     {
-      # code...
+      
     }
 
     //  ===================
