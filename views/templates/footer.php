23b6b4bc15e6529ecbc7eddb5aec41823405f298
ryfazrin <riansera515@gmail.com>
2021-08-12 23:11:22 +0800

    add footer template

---
+++ views/templates/footer.php
@@ -0,0 +1,10 @@
+<footer id="main-footer" class="text-center text-white bg-dark">
+	<div class="container">
+		<div class="row">
+			<div class="col">
+				<p class="footer-lead">Ver: 1.0.0 | Coded By: <a href="https://www.facebook.com/ryan.pazrin" target="_blank">Ryan Pazrin</a> - Kotabaru 2021</p>
+        Inspired By : <a href="https://www.facebook.com/adel.qusay.9" target="_blank">Adel Qusay</a>
+			</div>
+		</div>
+	</div>
+</footer>
