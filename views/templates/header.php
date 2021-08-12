f8ec191030386207794ca16624ee2d216391cea4
ryfazrin <riansera515@gmail.com>
2021-08-12 23:09:10 +0800

    add header template

---
+++ views/templates/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title><?= $title ?></title>
+
+	<!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="CI-4 Ryan Generator">
+
+  <!-- icon -->
+  <link rel="icon" type="image/x-icon" href="<?= BASE_URL ?>/assets/img/favicon.png" />		
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="<?= BASE_URL ?>/assets/dist/bootstrap/css/bootstrap.min.css">
+
+  <!-- Font Awesome CSS-->
+  <link rel="stylesheet" href="<?= BASE_URL ?>/assets/dist/font-awesome/css/font-awesome.min.css">
+  
+  <!-- App CSS -->
+  <link rel="stylesheet" href="<?= BASE_URL ?>/assets/css/app.css">
+</head>
+<body>
+
