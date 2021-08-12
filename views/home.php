1476a2c0ea404ba3a18d427367dec826ca62eeb3
ryfazrin <riansera515@gmail.com>
2021-08-12 23:23:06 +0800

    add home page

---
+++ views/home.php
@@ -0,0 +1,12 @@
+<?php include_once 'templates/header.php'; ?>
+
+<?php include_once 'templates/navbar.php'; ?>
+
+<div class="container">
+  <div class="text-center">
+    <h1>RYAN CI-4 CRUD Generator</h1>
+    <a href="<?= BASE_URL ?>/?p=generator" class="btn btn-success">Let's Create Your first CRUD</a>
+  </div>
+</div>
+
+<?php include_once 'templates/footer.php' ?>
